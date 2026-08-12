7dbe4b294d9625b5ebe9f750d636b04a391b2737
Prince Gupta <princegupta7698@gmail.com>
2026-08-12 13:04:30 +0530

    Add files via upload

--- unit2/7.py
+++ unit2/7.py
@@ -1,4 +1,10 @@
-nums = [1,2,3,4,5,6,7]
-sqr = [num ** 2 for num in nums]
+nums = [1,2,3,3,3,4,5,6,7]
 
-print(sqr)
+list_sqr = [num ** 2 for num in nums]
+set_sqr = { num ** 2 for num in nums}
+dict_sqr = {num: num ** 2 for num in nums}
+
+print("Original: ", nums)
+print("List comprehension: ",list_sqr)
+print("Set comprehension: ",set_sqr)
+print("Dict comprehension: ",dict_sqr)
