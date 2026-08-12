2cf974195a7aa5641f37daed479952031e2fcc06
Prince Gupta <princegupta7698@gmail.com>
2026-08-12 12:55:44 +0530

    Unit 3

---
+++ unit3/1.py
@@ -0,0 +1,11 @@
+import calculator as cal
+print(cal.add(10,2))
+
+# from calculator import *
+# print(add(10,2))
+
+# from calculator import add
+# print(add(10,2))
+
+from utilities.data import info
+print(info.s_data)
